871660c30444206bb67fdda3baadfbf1dba7feef
qhsong <sqh@sqh.me>
2016-10-25 13:43:50 +0800

    Add ssh-sysrq-test case

--- lib/kdump.sh
+++ lib/kdump.sh
@@ -14,6 +14,9 @@ K_CONFIG="/etc/kdump.conf"
 K_PATH="./KDUMP-PATH"
 K_RAW="./KDUMP-RAW"
 K_BACKUP_DIR="./backup"
+readonly K_LOCK_AREA="/root"
+readonly K_LOCK_SSH_ID_RSA=/root/.ssh/id_rsa_kdump_test
+readonly K_RETRY_COUNT=1000
 
 KPATH=${KPATH:-"${K_DEFAULT_PATH}"}
 OPTION=${OPTION:-}
@@ -65,7 +68,7 @@ prepare_kdump()
             log_info "- changing boot loader."
             {
                 /sbin/grubby    \
-                    --args="${KERARGS}"	\
+                    --args="${KERARGS}"    \
                     --update-kernel="${default}" &&
                 if [ "${K_ARCH}" = "s390x" ]; then zipl; fi
             } || {
@@ -144,22 +147,22 @@ label_fs()
 # append option to kdump.conf
 append_config()
 {
-    echo "- modifying /etc/kdump.conf"
+    log_info "- modifying /etc/kdump.conf"
     if [ $# -eq 0 ]; then
-        echo "- Nothing to append."
+        log_info "- Nothing to append."
         return 0
     fi
 
     while [ $# -gt 0 ]; do
-        echo "- removing existed old ${1%%[[:space:]]*} settings."
+        log_info "- removing existed old ${1%%[[:space:]]*} settings."
         sed -i "/^${1%%[[:space:]]*}/d" ${K_CONFIG}
-        echo "- adding new arguments '$1'."
+        log_info "- adding new arguments '$1'."
         echo "$1" >> "${K_CONFIG}"
         shift
     done
-    
-    echo "- show kdump.conf file."
-    cat "${K_CONFIG}"
+
+    log_info "- show kdump.conf file."
+    log_info $(grep -v ^# "${K_CONFIG}")
 }
 
 # config kdump.conf
@@ -173,54 +176,54 @@ configure_kdump_conf()
     # config_post, config_pre
     # config_extra
     # config_default
-    echo "- config kdump configuration"
+    log_info "- config kdump configuration"
     local dev=""
     local fstype=""
     local target=""
 
     # get dev, fstype
     if [ "yes" == "$RAW" -a -f "${K_RAW}" ]; then
-        dev=`cut -d" " -f1 ${K_RAW}`
-        fstype=`cut -d" " -f2 ${K_RAW}`
+        dev=$(cut -d" " -f1 ${K_RAW})
+        fstype=(cut -d" " -f2 ${K_RAW})
         rm -f ${K_RAW}
-        mkfs.$fstype $dev && mount $dev $MP
+        mkfs."${fstype[0]}" "$dev" && mount "$dev" "$MP"
     else
-        dev=`findmnt -kcno SOURCE $MP`
-        fstype=`findmnt -kcno FSTYPE $MP`
+        dev=$(findmnt -kcno SOURCE "$MP")
+        fstype=$(findmnt -kcno FSTYPE "$MP")
     fi
-
+    
     case $OPTION in
         uuid)
             # some partitions have both UUID= and PARTUUID=, we only want UUID=
-            target=`blkid $dev -o export -c /dev/null | grep '\<UUID='`
+            target=$(blkid "$dev" -o export -c /dev/null | grep '\<UUID=')
             ;;
         label)
-            target=`blkid $dev -o export -c /dev/null | grep LABEL=`
+            target=$(blkid "$dev" -o export -c /dev/null | grep LABEL=)
             if [ -z "$target" ]; then
-                label_fs $fstype $dev $MP $LABEL
-                target=`blkid $dev -o export -c /dev/null | grep LABEL=`
+                label_fs "$fstype" "$dev" "$MP" "$LABEL"
+                target=$(blkid "$dev" -o export -c /dev/null | grep LABEL=)
             fi
             ;;
         softlink)
-            ln -s $dev $dev-softlink
+            ln -s "$dev" "$dev-softlink"
             target=$dev-softlink
             ;;
         *)
             target=$dev
             ;;
     esac
-
+    
     if [ "yes" == "$RAW" -a -n "$target" ]; then
         append_config "raw $target"
         sed -i "/[ \t]\\$MP[ \t]/d" /etc/fstab
         echo "$dev $fstype" > ${K_RAW}
     elif [ -n "$fstype" -a -n "$target" ]; then
         append_config "$fstype $target" "path $KPATH"
-        mkdir -p $MP/$KPATH
+        mkdir -p "$MP/$KPATH"
         # tell crash analyse procedure where to find vmcore
         echo "${MP%/}${KPATH}" > ${K_PATH}
     else
-        echo "- Null dump_device/uuid/label or type wrong." && exit 1
+        log_error "- Null dump_device/uuid/label or type wrong."
     fi
 
 }
@@ -234,12 +237,219 @@ config_nfs_ipv6()
 {
     echo "config ipv6 nfs target"
 }
-
+#########################################
+# Prepare communication env for client and server
+# It will exit if program error
+# Param:
+#   $1 - Server Hostname
+#   $2 - Client Hostname
+# Global:
+#   None
+# Return:
+#   0 - Config success
+########################################
+prepare_for_multihost()
+{
+    which nc || yum install nmap-ncat || yum nc || log_error "Failed to install nc client"
+}
+#########################################
+# Config SSH-Kdump in server and client
+# When error trigger, It will exit
+# Param:
+#   $1 - Server Hostname
+#   $2 - Client Hostname
+# Global:
+#    
+# Return:
+#   0  -  Success config 
+#
+######################################
 config_ssh()
 {
-    echo "config ssh target"
+    yum install -y openssh-server openssh-clients || log_error "Failed to install openssh-server openssh-client"
+    local server
+    server=${SERVERS}
+    local client
+    client=${CLIENTS}
+    if [[ $(get_role) == "client" ]]; then  # copy certification
+        log_info "Run prepare_for_multihost as client"
+        mkdir -p "/root/.ssh"
+        cp ../lib/id_rsa ${K_LOCK_SSH_ID_RSA}
+        chmod 0600 ${K_LOCK_SSH_ID_RSA}
+        cp ../lib/id_rsa.pub "${K_LOCK_SSH_ID_RSA}.pub"
+        chmod 0600 "${K_LOCK_SSH_ID_RSA}.pub"
+
+        append_config "ssh root@${server}" || log_error "Error to config ssh target"
+        if [[ "$(grep -o '[0-9.]\+' /etc/redhat-release)" =~ ^6 ]]; then
+            append_config "link_delay 60"  # Some network interface, rhel 6 will wait network ready
+        fi
+
+        append_config "path ${K_DEFAULT_PATH}"
+        append_config "sshkey ${K_LOCK_SSH_ID_RSA}"
+        append_config "core_collector makedumpfile -l -F --message-level 1 -d 31"
+        log_info "Config Kdump file successful"
+
+        config_notify_at_client "${server}"
+        ssh -o StrictHostKeyChecking=no -i ${K_LOCK_SSH_ID_RSA} "${server}" 'touch ${K_LOCK_AREA}/ssh_test' || log_error "Test ssh connection error"
+        log_info "SSH connection build successful."
+    elif [[ $(get_role) == "server" ]]; then
+        log_info "Run prepare_for_multihost as server"
+        mkdir -p "/root/.ssh"
+        touch "/root/.ssh/authorized_keys"
+        cat ../lib/id_rsa.pub >> "/root/.ssh/authorized_keys"
+        restorecon -R "/root/.ssh/authorized_keys"
+
+        systemctl restart sshd || service sshd restart || log_error "Failed to restart sshd"
+        config_wait_at_server
+    else
+        log_error "Can not determine host role, Please check your hostname."
+    fi
+}
+################################
+# Only use at server side after config done
+# Param 
+#   None
+# Return
+#   0 - Wait successful
+#   1 - Timeout
+###############################
+config_wait_at_server()
+{
+    nc -l 35412
+}
+################################
+# Notify server config done after client config done
+# Param 
+#   $1 - Server Hostname
+#   $2 - Sync name
+# Return
+#   0 - Wait successful
+#   1 - Failed
+###############################
+config_notify_at_client()
+{
+    local count
+    count=${K_RETRY_COUNT}
+    local flag
+    flag=1
+    local server=$1
+    while [[ $count -gt 0 ]]; do  # repeate to dump data to server
+        echo "Success" > "/dev/tcp/${server}/35412"
+        if [[ $? -eq 0 ]]; then
+            flag=0
+            break
+        else
+            sleep 10s
+        fi
+        (( count = count - 1 ))
+    done
+
+    if [[ ${flag} -eq 1 ]]; then
+        log_error "Can not notificate server."
+    fi
+}
+#############################################
+# Only use at server side wait client trigger
+# Param
+#   None
+# Global
+#   None
+# Return
+#   None
+##############################################
+trigger_wait_at_server()
+{
+    nc -l 35413
+}
+############################################
+# Notify Trigger Finished to server
+# Param:
+#  $1 - server hostname
+# Global:
+#  None
+# Return
+#  None
+###########################################
+trigger_notify_at_client()
+{
+    local count
+    count=${K_RETRY_COUNT}
+    local flag
+    flag=1
+    local server=$1
+    while [[ $count -gt 0 ]]; do  # repeate to dump data to server
+        echo "Success" > "/dev/tcp/${server}/35413"
+        if [[ $? -eq 0 ]]; then
+            flag=0
+            break
+        else
+            sleep 10s
+        fi
+        (( count = count - 1 ))
+    done
+
+    if [[ ${flag} -eq 1 ]]; then
+        log_error "Can not notificate server, timeout."
+    fi
+
 }
 
+############################################
+# Check roles in multiple host task
+# Param:
+# Global:
+#   SERVERS - server ip or hostname
+#   CLIENTS - client ip or hostname
+# Return:
+#   server - Role is server
+#   client - Role is client 
+###########################################
+get_role()
+{
+    if ipcalc -c "${SERVERS}" &> /dev/null; then
+        if is_ip_match_host "${SERVERS}"; then
+            echo "server"
+            return
+        fi
+    else
+        if [[ "${SERVERS}" == "${HOSTNAME}" ]]; then
+            echo "server";return
+        fi
+    fi
+
+    if ipcalc -c "${CLIENTS}" &> /dev/null; then
+        if is_ip_match_host "${CLIENTS}"; then 
+            echo "client"
+            return
+        fi
+    else
+        if [[ "${CLIENTS}" == "${HOSTNAME}" ]]; then
+            echo "client"
+            return
+        fi
+    fi
+
+    log_error "Unable to determine roles, Please check your input."
+}
+############################################
+# Check IP parameter is current host IP
+# Param:
+#   1 - IP address
+# Global:
+# Return:
+#   0 - this IP is this host
+#   1 - this IP is not this host
+###########################################
+is_ip_match_host()
+{
+    local inputIP=$1
+    for ip in $(ip -o addr | awk '!/^[0-9]*: ?lo|link\/ether/ {gsub("/", " "); print $4}'); do
+        if [[ $ip == "${inputIP}" ]]; then
+            return 0
+        fi
+    done
+    return 1
+}
 config_ssh_key()
 {
     echo "config ssh key"
