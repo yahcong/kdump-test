3d0c901c84d2091ba5ae5483b092ba17d895ed7a
Zamir SUN <sztsian@gmail.com>
2016-12-04 21:12:38 +0800

    :bug: Use lib function to make ko in crash-altsysrq-c
    Fixes #28
    Replace load_altsysrq_driver() in crash-altsysrq-c with library method
    kdump.make_module() in crash-altsysrq-c
    
    Signed-off-by: Ziqian SUN <zsun@redhat.com>

--- crash-altsysrq-c/runtest.sh
+++ crash-altsysrq-c/runtest.sh
@@ -24,25 +24,13 @@
 
 C_REBOOT="./C_REBOOT"
 
-load_altsysrq_driver()
-{
-    mkdir altsysrq
-    cd altsysrq
-    cp ../altsysrq.c .
-    cp ../Makefile.altsysrq Makefile
-    unset ARCH
-    ( make && insmod ./altsysrq.ko )|| log_error "- make/insmod altsysrq module fail"
-    export ARCH
-    ARCH=$(uname -i)
-    cd ..
-}
-
 crash_altsysrq_c()
 {
     if [ ! -f ${C_REBOOT} ]; then
         kdump_prepare
         kdump_restart
-        load_altsysrq_driver
+        make_module "altsysrq"
+        insmod ./altsysrq/altsysrq.ko || log_error "- Fail to insmod altsysrq"
         touch "${C_REBOOT}"
         log_info "- boot to 2nd kernel"
         echo 1 > /proc/sys/kernel/sysrq
